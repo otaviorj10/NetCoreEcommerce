eff961897d7ba4598d735d673c00aa103e5b4bfe
Luiz Otavio <luiz_otavio71@hotmail.com>
2021-10-12 16:26:36 -0300

    Foi realizado o relacionamento entre as entidades e realizado nova migration

--- Aulas/Aula1/CasaDoCodigo/Migrations/20211012192409_Model.Designer.cs
+++ Aulas/Aula1/CasaDoCodigo/Migrations/20211012192409_Model.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CasaDoCodigo.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20211012192409_Model")]
+    partial class Model
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
