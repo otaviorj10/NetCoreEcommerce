a2710d4f2d5fe924fec67fed09078f2bf9892786
Otavio <luiz_otavio71@hotmail.com>
2021-10-12 11:16:21 -0300

    Asp.netCore Mvc parte 1 Ecommerce

---
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+
+        public IActionResult Carrossel()
+        {
+            return View();
+        }
+
+        public IActionResult Resumo ()
+        {
+            return View();
+        }
+
+
+    }
+}
